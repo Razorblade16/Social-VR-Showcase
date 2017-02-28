285a746ba9f45dd5adf39a06be011c4f50b8f87a
Andrew Bell <fiixed@gmail.com>
2017-02-28 17:48:10 -0600

    fixed audio on slides

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+
+    private GvrAudioSource audioSource;
+    // Use this for initialization
+
+    AudioClip clip;
+	void Start () {
+        audioSource = GetComponent<GvrAudioSource>();
+        clip = GetComponent<GvrAudioSource>().clip;
+	}
+
+    void HitByRay() {
+        audioSource.Play();
+    }
+
+    void Pause() {
+        audioSource.Pause();
+    }
+}
--- Assets/Scripts/VideoControl.cs
+++ Assets/Scripts/VideoControl.cs
@@ -8,12 +8,26 @@ public class VideoControl : MonoBehaviour {
     GameObject tempGameObject;
 
     void FixedUpdate() {
-        int layer_mask = LayerMask.GetMask("Video Player");
+        int layer_mask1 = LayerMask.GetMask("Video Player");
+        //int layer_mask2 = LayerMask.GetMask("Audio Player");
 
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, 1, layer_mask)) {
+        if (Physics.Raycast(ray, out hit, 1, layer_mask1) && rayHit == false) {
+            tempGameObject = hit.transform.gameObject;
+            tempGameObject.SendMessage("HitByRay");
+            rayHit = true;
+        } else {
+            if (rayHit) {
+                tempGameObject.SendMessage("Pause");
+                rayHit = false;
+            } else {
+                return;
+            }  
+        }
+
+        if (Physics.Raycast(ray, out hit, 1, layer_mask1)) {
             tempGameObject = hit.transform.gameObject;
             tempGameObject.SendMessage("HitByRay");
             rayHit = true;
@@ -24,8 +38,7 @@ public class VideoControl : MonoBehaviour {
             } else {
                 return;
             }
-            
         }
-        
+
     }
 }
